3c3e45c63c97a56fc733de35fa1fffec94804718
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-08-15 13:21:22 -0300

    instalados nova biblioteca e mudança no css

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,21 +1,22 @@
 import Colaborador from '../Colaborador'
+import hexToRgba from 'hex-to-rgba'
 import './time.css'
 
 const Time = ({ time, colaboradores, aoDeletar, mudaCor }) => {
     // desestruturação dos parametros recebidos
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
             <input
                 type='color'
                 className='input-cor'
-                value={time.corSecundaria}
+                value={time.cor}
                 onChange={(event) => mudaCor(event.target.value, time.nome)}
             />
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />
                 })}
             </div>
         </section>
